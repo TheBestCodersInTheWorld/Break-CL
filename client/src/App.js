74aabfd53fb44f707b59a8ddd9af7216e1925154
Ben Sandberg <bensandberg@Bens-Air.lan>
2023-12-03 17:35:13 -0600

    Started and made title, some customizations

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      {/* Hello world
+      <h1>Master Title</h1>
+      <h2>Smaller Title</h2>
+      <h3>Even Smaller</h3>
+
+      <p>woefhoqwjfowjeoifjawoi;ejfiojewOI;FJoiwejfoijojSNLKDVnlk</p>
+      <button>Click me</button>
+
+      <a>Link to...</a> */}
+      
+
+
+
+      <h1 className="Title">Break Soccer Champions League</h1>
+
+
+
+      
+    </div>
+  );
+}
+
+export default App;
