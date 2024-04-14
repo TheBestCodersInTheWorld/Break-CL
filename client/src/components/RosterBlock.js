a98180a941673c6157f966e9e82a90c0885eb68a
Ben Sandberg <bensandberg@Bens-Air.lan>
2024-04-14 17:31:43 -0500

    worked on rosters

--- client/src/components/RosterBlock.js
+++ client/src/components/RosterBlock.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../styles/Components.css"
 
 import Afc from "../images/afclogo.jpeg"
@@ -6,25 +6,41 @@ import Afc from "../images/afclogo.jpeg"
 const RosterBlock = (props) => {
   const logo = "../images/afclogo.jpeg"
 
-  const handleRoster = () => [
-    console.log("button pressed!")
-    
-  ]
+  // let teambutton = false;/
+  const [teambutton, setTeambutton] = useState(false)
+  // let [teambu
+
+  const handleRoster = () => {
+    setTeambutton(!teambutton)
+  }
 
   return (
-    <div className="RosterBlockContainer">
-      <div className="RosterButton" onClick={handleRoster} >Roster</div>
-      <div className="RosterTeam" >Team A</div>
-      <div className="RosterLogo" >
-        <img className="RosterImage" src={Afc} />
+    <div>
+      <div className="RosterBlockContainer">
+        <div className="RosterButton" onClick={handleRoster} >Roster</div>
+        <div className="RosterTeam" >{props.teamName}</div>
+        <div className="RosterLogo" >
+          <img className="RosterImage" src={Afc} />
+        </div>
+
+
       </div>
-      {/* <div className="RosterTeam" >{props.team}</div>
-      <div className="RosterLogo" >
-        <img src={props.teamLogo} />
-      </div> */}
+
+      {teambutton && <div className="RosterBlockList">
+        {
+          props.players.map((player) => 
+            <div className="RosterPlayer">{player.name}</div>
+          )
+        }
+       {/* <div className="RosterPlayer">(props.)</div>
+       <div className="RosterPlayer">Ian Byrne</div> */}
+      </div>}
+      
+
     </div>
+
+    
     
   )
 }
-
-export default RosterBlock
+export default RosterBlock;
--- client/src/pages/Rosters.js
+++ client/src/pages/Rosters.js
@@ -8,15 +8,80 @@ import '../styles/Rosters.css'
 
 
 const Rosters = () => {
+  const data = [
+    {
+      "teamName": "Los Cocos Locos FC",
+      "players": [
+        {
+          "name": "Ben Sandberg",
+        },
+        {
+          "name": "Ian Byrne",
+        }
+      ],
+    },
+    {
+      "teamName": "Asians FC",
+      "players": [
+        {
+          "name": "Ryan Vo",
+        },
+        {
+          "name": "Zach Estrera",
+        }
+      ]
+    },
+    {
+      "teamName": "Gufenshlachen FC",
+      "players": [
+        {
+          "name": "Nathaniel Dow",
+        },
+        {
+          "name": "George Hricik",
+        }
+      ]
+    },
+    {
+      "teamName": "Dingaling FC",
+      "players": [
+        {
+          "name": "Noah Prati",
+        },
+        {
+          "name": "Zaidan Gbadamosi",
+        }
+      ]
+    },
+    {
+      "teamName": "White Boys Elite FC",
+      "players": [
+        {
+          "name": "Hudson Peakes",
+        },
+        {
+          "name": "Wells Stimming",
+        }
+      ]
+    }
+  ]
+
+
+  
+
   return (
     <div className="Rosters"> 
       
       <Nav/>
       <div className="RosterContainer">
-        <RosterBlock/>
-        <RosterBlock/>
-        <RosterBlock/>
-        <RosterBlock />
+        {data.map((team, i) =>
+          <RosterBlock teamName={team.teamName} players={team.players} />
+        )}
+
+        {/* // <RosterBlock />
+        // <RosterBlock />
+        // <RosterBlock />
+        // <RosterBlock /> */}
       </div>
 
     
